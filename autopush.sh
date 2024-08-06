df904fc1dc34e54afb6c6b44eaee530f6ca74b28
samrouth1234 <samrouth1234>
2024-08-06 13:33:24 -0400

    update

--- autopush.sh
+++ autopush.sh
@@ -1,12 +1,12 @@
 git add .
 echo "==========================================="
-read -p "Please enter commit 🫠:" commit 
+read -p "Please enter commit 🫠   :" commit 
 echo "==========================================="
 git commit -m "$commit"
 echo "==========================================="
-read -p "Please Enter your branch name 🥺: " branch 
+read -p "Please Enter your branch name 🥺 : " branch 
 echo "==========================================="
 git push origin $branch
 
-echo "==========😍😍😍 ❤️ 😍😍😍============="
+echo "==========😍😍😍 ❤️  😍😍😍============="
 echo "Git push to $branch completed successfully!😒"
